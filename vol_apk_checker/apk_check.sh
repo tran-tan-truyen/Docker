7f419f7d437f23101dc5a6fbe8f2c5827a305a65
Truyen Tran <truyen.tran@voluntis.com>
2019-02-24 15:09:22 +0000

    first version

---
+++ vol_apk_checker/apk_check.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+cd /home
+
+for file in /data/*.apk; do
+  buffer+=$( echo -e "**** Data for $file ****")
+  cp $file working.zip
+  unzip -q working.zip
+  rm working.zip
+  find -name "* *" -type d | rename 's/ /_/g'    # do the directories first
+  find -name "* *" -type f | rename 's/ /_/g'
+  
+  JSONFILE=$(find . -name "*.json")
+
+  buffer+="\nJSON files:\n-----------"
+  for f in ${JSONFILE[@]}
+  do
+    buffer+=$(echo -e "\n$f\n")
+    buffer+=$(cat $f)
+  done
+
+  CertListCer=$(find . -name "*.cer")
+  CertListCrt=$(find . -name "*.crt")
+  CertListBks=$(find . -name "*.bks")
+
+  buffer+="\n\nCertificates:\n-------------"
+  for c in ${CertListCer[@]} ; do
+    buffer+=$(echo  -e "\n$c")
+    test=$(grep BEGIN $c | wc -l)
+    if [ $test -eq 0 ]
+    then
+      cert=$(echo "-----BEGIN CERTIFICATE-----\n")
+      cert+=$(base64 $c)
+      cert+=$(echo "\n-----END CERTIFICATE-----")
+	  d=$( echo -e $cert | openssl x509 -noout -enddate 2>&1  | sed 's/^.*=\(.*\)$/\1/g')
+	  endDate=$(date -d "$d" +%Y-%m-%d)
+	  buffer+=$( echo -e " - $endDate")
+      buffer+=$( echo -e $cert | openssl x509 -noout -subject 2>&1 | sed 's/^.*=\(.*\)$/ - \1/g')
+    else 
+	  d=$(cat $c | openssl x509 -noout -enddate 2>&1  | sed 's/^.*=\(.*\)$/\1/g')
+	  endDate=$(date -d "$d" +%Y-%m-%d)
+	  buffer+=$( echo -e " - $endDate")
+      buffer+=$(cat $c | openssl x509 -noout -subject 2>&1  | sed 's/^.*=\(.*\)$/ - \1/g')
+    fi
+  done
+
+  for c in ${CertListCrt[@]} ; do
+    buffer+=$(echo  -e "\n$c - ")
+    test=$(grep BEGIN $c | wc -l)
+    if [ $test -eq 0 ]
+    then
+      cert=$(echo "-----BEGIN CERTIFICATE-----\n")
+      cert+=$(base64 $c)
+      cert+=$(echo "\n-----END CERTIFICATE-----")
+	  d=$( echo -e $cert | openssl x509 -noout -enddate 2>&1 | sed 's/^.*=\(.*\)$/\1/g')
+	  endDate=$(date -d "$d" +%Y-%m-%d)
+	  buffer+=$( echo -e " - $endDate")
+      buffer+=$( echo -e $cert | openssl x509 -noout -subject 2>&1 | sed 's/^.*=\(.*\)$/ - \1/g')
+    else 
+	  d=$(cat $c | openssl x509 -noout -enddate 2>&1  | sed 's/^.*=\(.*\)$/\1/g')
+	  endDate=$(date -d "$d" +%Y-%m-%d)
+	  buffer+=$( echo -e " - $endDate")
+      buffer+=$(cat $c | openssl x509 -noout -subject 2>&1  | sed 's/^.*=\(.*\)$/ - \1/g')
+    fi
+  done
+  for c in ${CertListBks[@]} ; do
+    buffer+=$(echo  -e "\n$c ")
+	d=$(keytool -list -v -keystore $c -providerClass org.bouncycastle.jce.provider.BouncyCastleProvider -providerPath "/usr/share/java/bcprov.jar" -providerName BC -storetype BKS -storepass "" | grep until | sed  "s/.*until: \(.*\)$/\1/g" )
+    endDate=$(date -d "$d" +%Y-%m-%d)
+    buffer+=" - $endDate"
+    buffer+=$(keytool -list -v -keystore $c -providerClass org.bouncycastle.jce.provider.BouncyCastleProvider -providerPath "/usr/share/java/bcprov.jar" -providerName BC -storetype BKS -storepass "" | grep Owner | sed  "s/.*CN=\(.*\)$/ - \1/g" )
+  done
+  buffer+=$"\n***********************************************************\n"
+  rm -rf *
+done
+
+echo -e  "$buffer"
